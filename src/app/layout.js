85905fba4fed9e3ad1c01a7606b367336fef1150
pippoide <giovannipoma28@gmail.com>
2024-10-20 02:28:46 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
 import Head from "next/head";
-import { Metadata } from 'next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +9,7 @@ export const metadata = {
   title: "Folianator3000",
   description: "La Prof. Caproni ti ha datto da fare la foliazione ma non sai come fare? tranquillo qui ho ideato un algoritmo che lo farà al posto tuo, tanto fra 5 anni verremo sostituiti dall'IA ",
   authors: [{ name: 'Filippo Poma' }],
+  metadataBase: new URL('https://folianator3000.vercel.app/'),
   openGraph: {
     title: 'Folianator3000',
     description: "La Prof. Caproni ti ha datto da fare la foliazione ma non sai come fare? tranquillo qui ho ideato un algoritmo che lo farà al posto tuo, tanto fra 5 anni verremo sostituiti dall'IA ",
@@ -24,6 +24,9 @@ export const metadata = {
     url: 'https://folianator3000.vercel.app/', // URL della tua app
     type: 'website',
   },
+  icons: {
+    icon: '/favicon.ico', // Percorso della favicon
+  }
 };
 
 export default function RootLayout({ children }) {
